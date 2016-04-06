e811ee01319a50751083a5e2bd6125b286033a26
Kate Ramsey <kateramsey@live.com>
2016-04-06 15:43:18 -0500

    user/hello route uses a cookie

---
+++ Students/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Hello()
+        {
+            if (Request.Cookies["name"] != null)
+            {
+                var name = Request.Cookies["name"].ToString();
+                User user = new User() {Name = name};
+                return View(user);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Hello(User user)
+        {
+            //Write a cookie.
+            HttpCookie aCookie = new HttpCookie("name");
+            aCookie.Value = user.Name;
+            Response.Cookies.Add(aCookie);
+            return View(user);
+        }
+    }
+}
