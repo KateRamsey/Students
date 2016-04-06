c7cb16df5dddfe81b541b165b1070830ab6466a7
Kate Ramsey <kateramsey@live.com>
2016-04-06 13:43:18 -0500

    Add StudentController with required actions

---
+++ Students/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student newStudent)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int stuId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student editStudent)
+        {
+            return View();
+        }
+    }
+}
